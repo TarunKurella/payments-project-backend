7739272b6113874630b6af7dc2c3ee8dca8befb8
TronoKurelz <34759498+TarunKurella@users.noreply.github.com>
2021-09-23 18:36:09 +0530

    Update TransactionService.java

--- src/main/java/com/dbs/service/TransactionService.java
+++ src/main/java/com/dbs/service/TransactionService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.dbs.beans.Transaction;
 import com.dbs.repo.TransactionRepo;
+import com.dbs.beans.Account;
 
 @Service
 public class TransactionService {
@@ -12,7 +13,13 @@ public class TransactionService {
 	@Autowired
 	TransactionRepo transactionRepo;
 	
+	@Autowired
+	AccountRepo accountRepo;
+	
 	public Transaction addTransaction(Transaction transaction) {
+		Account account=accountRepo.findById(transaction.getFromAccount());
+		account.setClearBalance(account.getClearBalance()-((transaction.getAmount()*125)/100));
+		accountRepo.save(account);
 		return transactionRepo.save(transaction);
 	}
 
